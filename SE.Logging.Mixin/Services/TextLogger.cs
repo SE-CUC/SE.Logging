1aa10919639450ec7e40a4b7a866b7c7906a347f
k-edr <yevhenpuzenko@gmail.com>
2025-05-17 15:28:54 +0200

    added dufault logger, added "add and use"

---
+++ SE.Logging.Mixin/Services/TextLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngameScript
+{
+    public class TextLogger : ILoggable
+    {
+        private readonly StringBuilder _builder = new StringBuilder(50,5000);
+
+        public void Clear() => _builder.Clear();
+
+        public void Log(string message) => _builder.Append(message);
+
+        public void LogLine(string message) => _builder.AppendLine(message);
+
+        public override string ToString() => _builder.ToString();
+    }
+}
